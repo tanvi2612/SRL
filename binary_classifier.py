1794e8c2929831ec2fbf47e1885df46b6790a4dd
Tanvi Kamble <tanvi.kamble.2000@gmail.com>
2020-04-04 02:26:25 +0530

    added dataset

--- binary_classifier.py
+++ binary_classifier.py
@@ -2,17 +2,27 @@ import pandas as pd
 import prettytable as pt
 import numpy as np 
 from tabulate import tabulate
-table = pd.read_table('HDTB_pre_release_version-0.05/IntraChunk/CoNLL/utf/conversation/file-andhe_ki_lathi-1507111038.dat', skiprows=0, names=['ID', 'FORM', 'LEMMA', 'CPOSTAG', 'POSTAG', 'FEATS', 'HEAD', 'DEPREL', 'PHEAD', 'PDEPREL'])
+import os
+
+files = [os.path.join(path, name) for path, subdirs, files in os.walk("dataset") for name in files]
+
+write_file = open("data.csv", "a")
+
+for f in files:
+    table = pd.read_table(f, skiprows=0, names=['ID', 'FORM', 'LEMMA', 'CPOSTAG', 'POSTAG', 'FEATS', 'HEAD', 'DEPREL', 'PHEAD', 'PDEPREL'])
 
 
-# print(tabulate(table, headers=['ID','FORM','LEMMA','CPOSTAG','POSTAG','FEATS','HEAD','DEPREL','PHEAD','PDEPREL'], showindex=False))
-# print(table["FORM"])
-feature_array = table[['FEATS']].to_numpy()
-np.savetxt('features.txt', feature_array, fmt="%s", delimiter='|', newline='\n', header='', footer='', comments='# ', encoding=None)
+    # print(tabulate(table, headers=['ID','FORM','LEMMA','CPOSTAG','POSTAG','FEATS','HEAD','DEPREL','PHEAD','PDEPREL'], showindex=False))
+    # print(table["FORM"])
+    feature_array = table[['FEATS']].to_numpy()
+    np.savetxt('features.txt', feature_array, fmt="%s", delimiter='|', newline='\n', header='', footer='', comments='# ', encoding=None)
 
-# print(feature_array)
-features = pd.read_csv('features.txt', sep='|', skiprows=0)
+    # print(feature_array)
+    features = pd.read_csv('features.txt', sep='|', skiprows=0, names=['cat', 'gen', 'num', 'pers', 'case', 'vib', 'tam', 'chunkId', 'chunkType' , 'stype' , 'voicetype'])
 
-final = pd.concat([table, features], axis = 1)
+    final = pd.concat([table, features], axis = 1)
+    write_in = np.concatenate([table[['ID', 'FORM', 'LEMMA', 'HEAD', 'DEPREL']].to_numpy(), features.to_numpy()], axis=1)
+    print((write_in))
+    np.savetxt(write_file, write_in, fmt='%s')
+    # print(tabulate(final,  showindex=False))
 
-print(tabulate(final,  showindex=False))
