5fbfcf16dcb604b7470c6d5f6d17d1fa5e86a0dd
sumba101 <sumanth.goku@gmail.com>
2020-04-26 20:32:36 +0530

    finished stage 1

--- code/binary_classifier.py
+++ code/binary_classifier.py
@@ -1,28 +1,58 @@
 import pandas as pd
 import numpy as np
-
-filepath="./out.csv"
-dataset = pd.read_csv(filepath)
-vectors=dataset.drop('Labels',axis=1)
-target=dataset['Labels']
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(vectors, target, test_size = 0.30)
-
-from sklearn.svm import SVC
-svclassifier = SVC(kernel='linear')
-svclassifier.fit(X_train, y_train)
-
+import sys
+from sklearn.preprocessing import LabelEncoder
+import joblib
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+num=int(sys.argv[1])
+
+filepath="./expected.csv"
+dataset = pd.read_csv(filepath)
+
+#the different parameters to test with
+temp=['chunk','postposition','head-POS','dependency-head','dependency']
+
+for curr in temp:
+    dropped=curr
+
+    dropped_fields=['Label','chunk','srl','postposition','head-POS','dependency','dependency-head','predicate']
+    dropped_fields.remove(dropped)
+
+    #remove the current parameter from the parameters that are to be removed
+    
+    for i in range(0,300):
+        dropped_fields.append('dim_{}'.format(i))
+    
+    #add all the vector dimensions that must be removed
+
+    vectors=dataset.drop(dropped_fields,axis=1)
+    target=dataset['Label']
+
+    #sets up the target and the parameters used for the predictions
+    
+    vals=vectors[dropped].values
+    vectors[dropped]=LabelEncoder().fit_transform(vals)
+
+    #Converts the strings to number representations     
+    
+    X_train, X_test, y_train, y_test = train_test_split(vectors, target, test_size = 0.30)
+
+    svclassifier = SVC(kernel='linear')
+    svclassifier.fit(X_train, y_train)
 
 
-from sklearn.externals import joblib
+    y_pred=svclassifier.predict(X_test)
 
-joblib_file = "binary_model.pkl"
-joblib.dump(svclassifier, joblib_file)
 
-# Load from file
-# joblib_model = joblib.load(joblib_file)
+    with open('outputs/console{}.txt'.format(num),'w') as f:
+        print(confusion_matrix(y_test,y_pred),file=f)
+        print(classification_report(y_test,y_pred),file=f)
+
+    joblib_file = "models/binary_model{}.pkl".format(num)
+    joblib.dump(svclassifier, joblib_file)
+    
+    num+=5
+
